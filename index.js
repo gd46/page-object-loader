224386caef2ce050d4d2168dfcb70ab3e7eb0bda
DiBella, Giuseppe <Giuseppe.DiBella@ADP.com>
2017-09-01 07:11:43 -0400

    Update to use glob.sync instead

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
 let glob = require("glob");
 let _ = require('lodash');
+let path = require('path');
+let pages = {};
 
-glob("**/*.page.js", {
-  nonull: true
-}, (er, filePaths) => {
-  let pages = {};
-  filePaths.forEach((filePath) => {
-  	let fileName = filePath.split(/(\\|\/)/g).pop(); // extract file name with extension
-  	let pageName = _.camelCase(fileName.split('.', 1)[0]); // create the file name pieces and take the first which is the name and format it
-    let pageObjectName = pageName.charAt(0).toUpperCase() + pageName.slice(1); // make the file name match the name of the page object function or class
-    pages[pageObjectName] = require(`./${filePath}`);
+  let filePaths = glob.sync("**/*.page.js", {
+    nonull: true
   });
 
-  let welcome = new pages.Welcome();
+  filePaths.forEach((filePath) => {
+      let fileName = filePath.split(/(\\|\/)/g).pop(); // extract file name with extension
+      let pageName = _.camelCase(fileName.split('.', 1)[0]); // create the file name pieces and take the first which is the name and format it
+      let pageObjectName = pageName.charAt(0).toUpperCase() + pageName.slice(1); // make the file name match the name of the page object function or class
+      pages[pageObjectName] = require(path.resolve(`${filePath}`));
+    });
+    console.log('pages', pages);
 
-  console.log('pages', pages);
-  console.log('welcome page', welcome);
-  console.log('wecome function', welcome.getWelcomeMsg());
-});
+module.exports = pages;
