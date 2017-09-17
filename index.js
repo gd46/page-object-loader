3f6901f83ccd5a5cce614ec266c810bbc85fbbe9
gd46 <gd46@njit.edu>
2017-09-17 19:15:28 -0400

    Update loader logic

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
-let glob = require("glob");
+let glob = require('glob');
 let _ = require('lodash');
 let path = require('path');
 let pages = {};
 
-  let filePaths = glob.sync("**/*.page.js", {
+  let filePaths = glob.sync('**/*.page.js', {
     nonull: true
   });
 
   filePaths.forEach((filePath) => {
-      let fileName = filePath.split(/(\\|\/)/g).pop(); // extract file name with extension
-      let pageName = _.camelCase(fileName.split('.', 1)[0]); // create the file name pieces and take the first which is the name and format it
-      let pageObjectName = pageName.charAt(0).toUpperCase() + pageName.slice(1); // make the file name match the name of the page object function or class
+      // extract file name without the extension
+      let fileName =  path.basename(filePath, '.js');
+      /*
+        create the file name pieces and take the first 
+        which is the name and format it
+      */
+      let pageName = _.camelCase(fileName.split('.', 1)[0]);
+      // make the file name match the name of the page object function or class
+      let pageObjectName = pageName.charAt(0).toUpperCase() + pageName.slice(1);
       pages[pageObjectName] = require(path.resolve(`${filePath}`));
     });
-    console.log('pages', pages);
 
 module.exports = pages;
